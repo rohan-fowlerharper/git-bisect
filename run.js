6d15df0829225fc908506ca4139ebc72e306951a
Rohan Fowler-Harper <88605753+rohan-fowlerharper@users.noreply.github.com>
2021-11-02 13:00:20 +1300

    update console log

--- run.js
+++ run.js
@@ -4,8 +4,8 @@ const array = require('./array')
 
 const userInput = Number(process.argv[2])
 
-const [ value1, index1, numIterations1 ] = binarySearch(array, Number(process.argv[2]))
-const [ value2, index2, numIterations2 ] = linearSearch(array, Number(process.argv[2]))
+const [ value1, index1, numIterations1 ] = binarySearch(array, userInput)
+const [ value2, index2, numIterations2 ] = linearSearch(array, userInput)
 
 console.log(`Binary Search - Value found: ${value1} at index: ${index1}, Iterations: ${numIterations1}`)
 console.log(`Linear Search - Value found: ${value2} at index: ${index1}, Iterations: ${numIterations2}`)
